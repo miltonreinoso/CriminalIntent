a89dfb8f93bfbe520abd2b1b4fce7238332596a0
milton <reinoso.vm@gmail.com>
2018-07-11 19:14:54 -0300

    Add DateButton, Land Layout, and checkbox changing to Big Nerd Ranch V3
    Signed-off-by: milton <reinoso.vm@gmail.com>

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -9,12 +9,20 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+
+import java.text.DateFormat;
 
 public class CrimeFragment extends Fragment {
 
     private Crime mCrime;
     private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
+    private DateFormat mDF = DateFormat.getDateInstance(DateFormat.FULL );
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -45,6 +53,19 @@ public class CrimeFragment extends Fragment {
             @Override
             public void afterTextChanged(Editable editable) {
                 //blank intentionally
+            }
+        });
+
+        mDateButton = (Button)v.findViewById(R.id.crime_date);
+        mDateButton.setText(mDF.format(this.mCrime.getDate()));
+        mDateButton.setEnabled(false);
+
+        mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            // Set the crime's solved property
+                mCrime.setSolved(isChecked);
             }
         });
 
